dd73b6dbcd85abbc94704e6b842368bd4f53b728
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 17:51:35 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio9/Program.cs
@@ -0,0 +1,11 @@
+﻿//Crea un array e inserta los números pares del 1 al 100 en ese array.
+int[] numerosPares = new int[50];
+int k = 0;
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0)
+    {
+        numerosPares[k] = i;
+        k++;
+    }
+}
